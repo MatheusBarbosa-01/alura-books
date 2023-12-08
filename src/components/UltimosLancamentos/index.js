2e42429224b41c70c7493f04509832712a44d60a
Matheus <matheusbnvr@gmail.com>
2023-12-08 12:51:24 -0300

    Criação de recomendações
    Foi criado um componente de card para abrigar as recomendações. Foi utilizado Props.

--- src/components/UltimosLancamentos/index.js
+++ src/components/UltimosLancamentos/index.js
@@ -1,7 +1,9 @@
 import { livros } from './dadosUltimosLancamentos'
 import { Titulo } from '../Titulo'
 import CardRecomenda from '../CardRecomenda'
+import imagemLivro from '../../img/livro2.png'
 import styled from 'styled-components'
+
 
 const UltimosLancamentosContainer = styled.section`
 background-color: #EBECEE;
@@ -22,19 +24,20 @@ cursor: pointer;
     }
 `
 
-export default function UltimosLancamentos(){
-    return(
+export default function UltimosLancamentos() {
+    return (
         <UltimosLancamentosContainer>
             <Titulo cor="#EB9B00" tamanhoFonte="36px">ÚLTIMOS LANÇAMENTOS!</Titulo>
             <NovosLivrosContainer>
-                {livros.map (livro => (
-                    <img src={livro.src}/>
+                {livros.map(livro => (
+                    <img src={livro.src} />
                 ))}
             </NovosLivrosContainer>
             <CardRecomenda
-            titulo="Talvez você se interesse por"
-            subtitulo="Angular 11"
-            descricao="Construindo uma aplicação com a plataforma Google."
+                titulo="Talvez você se interesse por"
+                subtitulo="Angular 11"
+                descricao="Construindo uma aplicação com a plataforma Google."
+                img={imagemLivro}
             />
         </UltimosLancamentosContainer>
     )
